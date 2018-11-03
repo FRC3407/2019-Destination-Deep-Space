426c7f5f7cd47a722c36b38722d17416de5c2108
Kelton O'Brien <Keltonpobrien@gmail.com>
2018-11-03 13:59:51 -0500

    add Auto

--- src/main/java/frc/robot/commands/DefaultAuto.java
+++ src/main/java/frc/robot/commands/DefaultAuto.java
@@ -1,87 +1,26 @@
 package main.java.frc.robot.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
 
-public class DefaultAuto extends Command {
+public class DefaultAuto extends CommandGroup {
+
     public DefaultAuto() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    }
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+        addSequential(new TimedDrive(4 , 0.5, 0.5));
+        addSequential(new TimedDrive(1,0.1,0.7));
 
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
 
-    /**
-     * The initialize method is called just before the first time
-     * this Command is run after being started.
-     */
-    @Override
-    protected void initialize() {
-
-    }
-
-
-    /**
-     * The execute method is called repeatedly when this Command is
-     * scheduled to run until this Command either finishes or is canceled.
-     */
-    @Override
-    protected void execute() {
-
-    }
-
-
-    /**
-     * <p>
-     * Returns whether this command is finished. If it is, then the command will be removed and
-     * {@link #end()} will be called.
-     * </p><p>
-     * It may be useful for a team to reference the {@link #isTimedOut()}
-     * method for time-sensitive commands.
-     * </p><p>
-     * Returning false will result in the command never ending automatically. It may still be
-     * cancelled manually or interrupted by another command. Returning true will result in the
-     * command executing once and finishing immediately. It is recommended to use
-     * {@link edu.wpi.first.wpilibj.command.InstantCommand} (added in 2017) for this.
-     * </p>
-     *
-     * @return whether this command is finished.
-     * @see Command#isTimedOut() isTimedOut()
-     */
-    @Override
-    protected boolean isFinished() {
-        // TODO: Make this return true when this Command no longer needs to run execute()
-        return false;
-    }
-
-
-    /**
-     * Called once when the command ended peacefully; that is it is called once
-     * after {@link #isFinished()} returns true. This is where you may want to
-     * wrap up loose ends, like shutting off a motor that was being used in the
-     * command.
-     */
-    @Override
-    protected void end() {
-
-    }
-
-
-    /**
-     * <p>
-     * Called when the command ends because somebody called {@link #cancel()} or
-     * another command shared the same requirements as this one, and booted it out. For example,
-     * it is called when another command which requires one or more of the same
-     * subsystems is scheduled to run.
-     * </p><p>
-     * This is where you may want to wrap up loose ends, like shutting off a motor that was being
-     * used in the command.
-     * </p><p>
-     * Generally, it is useful to simply call the {@link #end()} method within this
-     * method, as done here.
-     * </p>
-     */
-    @Override
-    protected void interrupted() {
-        super.interrupted();
+        // A command group will require all of the subsystems that each member would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the arm.
     }
 }
