de936ce45e07ef619aecb80c7cab19e575948e88
jsthompson49 <jsthompson049@gmail.com>
2019-02-06 20:30:25 -0600

    Add explict Main which is needed this year

---
+++ src/frc/team3407/Main.java
@@ -0,0 +1,10 @@
+package frc.team3407;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+public class Main {
+
+    public static void main(String... args) {
+        RobotBase.startRobot(Robot::new);
+    }
+}
